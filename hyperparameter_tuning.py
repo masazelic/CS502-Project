91ff3a9a3abc2022c950fc71a73307981ceec308
elena-mrdja <91255937+elena-mrdja@users.noreply.github.com>
2023-12-11 12:23:30 +0100

    Add files via upload

---
+++ hyperparameter_tuning.py
@@ -0,0 +1,108 @@
+from methods.relationnet import RelationNet
+from datasets.prot.swissprot import *
+from datasets.cell.tabula_muris import *
+from backbones.fcnet import FCNet, FCNetRN
+
+def get_dataset(n_way, n_support, n_query, dataset='swissprot'):
+    # Since RelationNet is meta based approach
+    if dataset == 'swissprot':
+        train_dataset = SPSetDataset(n_way=n_way, n_support=n_support, n_query=n_query, n_episode=100, mode='train')
+        val_dataset = SPSetDataset(n_way=n_way, n_support=n_support, n_query=n_query, mode='val')
+    else:
+        train_dataset = TMSetDataset(n_way=n_way, n_support=n_support, n_query=n_query, n_episode=100, mode='train')
+        val_dataset = TMSetDataset(n_way=n_way, n_support=n_support, n_query=n_query, mode='val')
+    
+    train_loader = train_dataset.get_data_loader()
+    val_loader = val_dataset.get_data_loader()
+    
+    return train_dataset, val_dataset, train_loader, val_loader
+
+def initialize_model(learning_rate, backbone_layers, backbone_relation_layers, hidden_dim, train_dataset, n_way, n_support):
+    
+    backbone = FCNet(layer_dim=backbone_layers, x_dim=train_dataset.x_dim)
+    backbone_relation = FCNetRN(layer_dim=backbone_relation_layers, hidden_dim=hidden_dim, x_dim=backbone_layers[-1]*2)
+    model = RelationNet(n_way=n_way, n_support=n_support, backbone=backbone, backbone_relation=backbone_relation)
+    optimizer = torch.optim.Adam(lr=learning_rate, params=model.parameters())
+    
+    if torch.cuda.is_available():
+        model = model.cuda()
+    
+    return model, optimizer
+
+def train(train_loader, epoch, model, optimizer):
+    
+    for i in range(epoch):
+        model.train()
+        model.train_loop(epoch, train_loader, optimizer)
+
+
+
+def hyperparameter_tuning(n_way, n_support, n_query, dataset='swissprot'):
+
+    # Parameters we are going to vary 
+    learning_rate = [0.01, 0.01, 0.0001]
+    epochs = [40, 60, 80]
+    backbone_layers = [[512, 512], [512], [128], [64, 64]]
+    backbone_relation_layers = [[256, 256], [256, 64], [128]]
+    hidden_dim = [128, 64, 32]
+    
+    best_mean_acc = -1
+    best_lr = -1
+    best_epoch = -1
+    best_bb_lyr = []
+    best_bb_rel_lyr = []
+    best_hd = -1
+    for lr in learning_rate:
+        for epoch in epochs:
+            for bb_lyr in backbone_layers:
+                for bb_rel_lyr in backbone_relation_layers:
+                    for hd in hidden_dim:
+                        average_mean_acc = 0.0
+                        # For one fold take train and validation splits
+                        train_dataset, _, train_loader, val_loader = get_dataset(n_way, n_support, n_query, dataset)
+                        print("Curently evaluating these parameters:")
+                        print("Learning rate:", lr)
+                        print("Epochs:", epoch)
+                        print("Backbone layers:", bb_lyr)
+                        print("Backbone relation layers:", bb_rel_lyr)
+                        print("Hidden dim:", hd)
+                            
+                        model, optimizer = initialize_model(lr, bb_lyr, bb_rel_lyr, hd, train_dataset, n_way, n_support)
+                            
+                        # First, we are going to train the model on one training dataset
+                        train(train_loader, epoch, model, optimizer)
+                            
+                        # Then, we are going to evaluate it on one validation dataset
+                        acc_mean, acc_std = model.test_loop(val_loader, return_std=True)
+                        
+                        # If we have better average mean accuracy 
+                        if acc_mean > best_mean_acc:
+                            best_mean_acc = acc_mean
+                            # Saving the parameters of the best model till now
+                            best_lr = lr
+                            best_epoch = epoch
+                            best_bb_lyr = bb_lyr
+                            best_bb_rel_lyr = bb_rel_lyr
+                            best_hd = hd
+                            
+    print("Best hyperparameters:")
+    print("Learning rate:", lr)
+    print("Epochs:", epoch)
+    print("Backbone layers:", bb_lyr)
+    print("Backbone relation layers:", bb_rel_lyr)
+    print("Hidden dim:", hd)
+    
+if __name__ == '__main__':
+    hyperparameter_tuning(5, 5, 15, 'swissprot')
+                        
+                       
+                            
+                            
+                            
+                            
+        
+    
+    
+    
+    
+    
